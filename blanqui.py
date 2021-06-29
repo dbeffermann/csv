8df02845bfd392de2dbbafe02cd777342b194b18
Daniel Beffermann <dbeffermannj@gmail.com>
2021-06-29 14:56:05 -0400

    fixed

--- blanqui.py
+++ blanqui.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
 import base64
+import datatable as dt
 
 archivo = st.file_uploader('Csv Mañoso')
 
 if archivo:
-    df = dt.fread("catalog_products (2).csv").to_pandas()
+    df = dt.fread(archivo).to_pandas()
     df['visible'] = df['visible'].apply(lambda x: str(x).upper())
     csv = df.to_csv(index=False, sep=',')
     b64 = base64.b64encode(csv.encode()).decode()
