c6d9e0b5524de7a5df65cb37d72947bb9965cb03
Daniel Beffermann <dbeffermannj@gmail.com>
2021-06-29 13:59:40 -0400

    update sep

--- blanqui.py
+++ blanqui.py
@@ -5,7 +5,10 @@ import base64
 archivo = st.file_uploader('Csv Mañoso')
 
 if archivo:
-    df = pd.read_csv(archivo, sep = ';')
+    try:
+        df = pd.read_csv(archivo, sep = ';')
+    except:
+        df = pd.read_csv(archivo, sep = ',')
     df['visible'] = df['visible'].apply(lambda x: str(x).upper())
     csv = df.to_csv(index=False, sep=',')
     b64 = base64.b64encode(csv.encode()).decode()
