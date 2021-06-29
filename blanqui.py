da0c733ab578c2352878531d7a671f00c354c103
Daniel Beffermann <dbeffermannj@gmail.com>
2021-06-29 15:26:08 -0400

    updated

--- blanqui.py
+++ blanqui.py
@@ -6,7 +6,7 @@ import base64
 archivo = st.file_uploader('Csv Mañoso')
 
 if archivo:
-    pd.read_csv("catalog_products (2).csv", sep = '\t', encoding = 'latin1')
+    pd.read_csv(archivo, sep = '\t', encoding = 'latin1')
     df['visible'] = df['visible'].apply(lambda x: str(x).upper())
     csv = df.to_csv(index=False, sep=',')
     b64 = base64.b64encode(csv.encode()).decode()
